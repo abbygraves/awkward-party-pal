bec25b90376432e305ad1e2f66042650d93b3c1c
jbradley84 <utbradley84@yahoo.com>
2022-06-10 12:26:45 -0500

    registered copy button click event

--- assets/js/event.js
+++ assets/js/event.js
@@ -1,5 +1,6 @@
 let apiGenEl = document.querySelector("#api-generation");
 let kanyeQuoteBtn = document.querySelector("#kanye-btn");
+let copyBtn = document.querySelector("#copy");
 
 // function to get random Kanye West quotes
 function askKanye() {
@@ -12,12 +13,33 @@ function askKanye() {
     })
 }
 
+
+// function to display api response data
 function generateApiDiv(data) {
+    // clear existing quote
+    apiGenEl.innerHTML = "";
+
+    // dynamically generate p-tag with api response data
     let apiDiv = document.createElement("p");
     apiDiv.textContent = data.quote;
+    apiDiv.setAttribute("id", "copy");
+    // append p-tag to html
     apiGenEl.appendChild(apiDiv);
+}
 
+
+// function to add a copy of displayed api response data to the clipboard
+function copyClipboard(event) {
+    let copyClick = event.target.getAttribute("#copy");
+    console.log("click");
+    //let copyText = ;
+    //copyText.select(data.quote);
+    //navigator.clipboard.writeText(copyText);
+    //alert("Copied the text: " + copyText);
 }
 
 // event listenter to trigger askKanye function
 kanyeQuoteBtn.addEventListener("click", askKanye);
+
+// event listener to copy api response
+copyBtn.addEventListener("click", copyClipboard);
