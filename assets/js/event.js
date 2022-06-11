e43cf2ec4927542d6e5e2880b9969ab733412ade
jbradley84 <utbradley84@yahoo.com>
2022-06-11 14:17:41 -0500

    added function to display random fact on button click

--- assets/js/event.js
+++ assets/js/event.js
@@ -1,5 +1,6 @@
 let apiGenEl = document.querySelector("#api-gen");
 let kanyeQuoteBtn = document.querySelector("#kanye-btn");
+let tFtBtn = document.querySelector("#tFt-btn");
 let copyBtn = document.querySelector("#copy");
 
 // function to get random Kanye West quotes
@@ -7,20 +8,51 @@ function askKanye() {
     let apiUrl = "https://api.kanye.rest";
     fetch(apiUrl).then(function (response) {
         response.json().then(function (data) {
-            genApiDiv(data);
+            genKanyeDiv(data);
         })
     })
 }
 
 
-// function to display api response data
-function genApiDiv(data) {
+// function to get random this for that descriptions
+function randomFact() {
+    var limit = 1;
+    $.ajax({
+        method: 'GET',
+        url: 'https://api.api-ninjas.com/v1/facts?limit=' + limit,
+        headers: { 'X-Api-Key': 'APIItc6Clhr3spOTxL6yNw==gpXlH2wS5Go2YP5h' },
+        contentType: 'application/json',
+        success: function (result) {
+            genFactDiv(result);
+        },
+        error: function ajaxError(jqXHR) {
+            console.error('Error: ', jqXHR.responseText);
+        }
+    });
+}
+
+// function to display kanye response
+function genKanyeDiv(data) {
     // clear existing quote
     apiGenEl.innerHTML = "";
 
     // dynamically generate p-tag with api response data
     let apiDiv = document.createElement("p");
     apiDiv.textContent = data.quote;
+    apiDiv.setAttribute("id", "copy");
+    // append p-tag to html
+    apiGenEl.appendChild(apiDiv);
+}
+
+
+// function to display random fact response data
+function genFactDiv(result) {
+    // clear existing quote
+    apiGenEl.innerHTML = "";
+    
+    // dynamically generate p-tag with api response data
+    let apiDiv = document.createElement("p");
+    apiDiv.textContent = result[0].fact;
     apiDiv.setAttribute("id", "copy");
     // append p-tag to html
     apiGenEl.appendChild(apiDiv);
@@ -40,6 +72,9 @@ function copyClipboard(event) {
 
 // event listenter to trigger askKanye function
 kanyeQuoteBtn.addEventListener("click", askKanye);
+
+// event listener to trigger thisForThat function
+tFtBtn.addEventListener("click", randomFact);
 
 // event listener to copy api response
 copyBtn.addEventListener("click", copyClipboard);
