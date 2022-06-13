33acfaa3efdf5c1b7a0fab74eb0148e440533d97
Julian Heaney <julianpheaney@gmail.com>
2022-06-13 14:37:58 -0500

    saving for posterity

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,23 @@
-function saveProfile(name, iceBreakers) {
-    window.localStorage.setItem(name, iceBreakers);
+var formName=document.querySelector("#name")
+
+var formDetails=document.querySelector("#icebreaker")
+
+let saveBtn=document.querySelector("#submit");
+
+let guestArray=[]
+let guestIdCounter=0
+function saveProfile(event) {
+ //   window.localStorage.setItem(name, iceBreakers);
+ event.preventDefault();
+ var guestObject={
+    name:formName.value, 
+    details:formDetails.value
+ }
+guestObject.setAttribute("id", guestIdCounter);
+console.log(guestObject.id);
+ //guestArray.push(guestObject);
+ //console.log(guestArray);
+ //localStorage.setItem("guestInfo", JSON.stringify(guestObject));
 }
 
 function getProfiles() {
@@ -11,3 +29,5 @@ function getProfiles() {
 function clearProfiles() {
         localStorage.clear();
 }
+
+saveBtn.addEventListener("click", saveProfile);
