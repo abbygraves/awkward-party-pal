d0036807a49e297293e640acfbd6c869733fc368
Julian Heaney <julianpheaney@gmail.com>
2022-06-13 23:09:22 -0500

    updated JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,20 +4,34 @@ var formDetails=document.querySelector("#icebreaker")
 
 let saveBtn=document.querySelector("#submit");
 
-let guestArray=[]
-let guestIdCounter=0
+//let guestArray=[]
+//let guestIdCounter=0
 function saveProfile(event) {
  //   window.localStorage.setItem(name, iceBreakers);
+ var guestArrayString=localStorage.getItem("guestInfo");
+let guestArray=[]
+ if (guestArrayString!==null){
+guestArray=JSON.parse(guestArrayString);
+console.log(guestArrayString);
+}
+
+let guestIdCounter=localStorage.getItem("guestIdCounter") || 0;
+
  event.preventDefault();
  var guestObject={
     name:formName.value, 
-    details:formDetails.value
+    details:formDetails.value,
+    id:guestIdCounter
  }
-guestObject.setAttribute("id", guestIdCounter);
+ guestIdCounter++;
+//guestObject.setAttribute("id", guestIdCounter);
 console.log(guestObject.id);
- //guestArray.push(guestObject);
+ guestArray.push(guestObject);
  //console.log(guestArray);
- //localStorage.setItem("guestInfo", JSON.stringify(guestObject));
+ localStorage.setItem("guestInfo", JSON.stringify(guestArray));
+ localStorage.setItem("guestIdCounter", guestIdCounter);
+ formName.value="";
+ formDetails.value="";
 }
 
 function getProfiles() {
