5d06743f1aaad79a2e450582cb52b32d956b5d7f
Julian Heaney <julianpheaney@gmail.com>
2022-06-15 13:16:28 -0500

    small to clean up script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,44 +1,41 @@
-var formName=document.querySelector("#name")
-
-var formDetails=document.querySelector("#icebreaker")
-
-let saveBtn=document.querySelector("#submit");
+var formName = document.querySelector("#name")
+var formDetails = document.querySelector("#icebreaker")
+let saveBtn = document.querySelector("#submit");
 
 function saveProfile(event) {
+    var guestArrayString = localStorage.getItem("guestInfo");
+    let guestArray = []
+    if (guestArrayString !== null) {
+        guestArray = JSON.parse(guestArrayString);
+        console.log(guestArrayString);
+    }
 
- var guestArrayString=localStorage.getItem("guestInfo");
- let guestArray=[]
- if (guestArrayString!==null){
-    guestArray=JSON.parse(guestArrayString);
-    console.log(guestArrayString);
-}
+    let guestIdCounter = localStorage.getItem("guestIdCounter") || 0;
 
- let guestIdCounter=localStorage.getItem("guestIdCounter") || 0;
+    event.preventDefault();
+    var guestObject = {
+        name: formName.value,
+        details: formDetails.value,
+        id: guestIdCounter
+    }
 
- event.preventDefault();
-  var guestObject={
-    name:formName.value, 
-    details:formDetails.value,
-    id:guestIdCounter
- }
-
- guestIdCounter++;
- console.log(guestObject.id);
- guestArray.push(guestObject);
- localStorage.setItem("guestInfo", JSON.stringify(guestArray));
- localStorage.setItem("guestIdCounter", guestIdCounter);
- formName.value="";
- formDetails.value="";
+    guestIdCounter++;
+    console.log(guestObject.id);
+    guestArray.push(guestObject);
+    localStorage.setItem("guestInfo", JSON.stringify(guestArray));
+    localStorage.setItem("guestIdCounter", guestIdCounter);
+    formName.value = "";
+    formDetails.value = "";
 }
 
 function getProfiles() {
     Object.keys(localStorage).forEach((key) => {
         console.log(key + " " + localStorage.getItem(key));
-       });
+    });
 }
 
 function clearProfiles() {
-        localStorage.clear();
+    localStorage.clear();
 }
 
 saveBtn.addEventListener("click", saveProfile);
